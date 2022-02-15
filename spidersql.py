ed15267b5908f1bdc461d4feb8a289033f64270f
Nino <40154164+hwq37403@users.noreply.github.com>
2022-02-15 15:20:19 +0800

    HIDE

--- spidersql.py
+++ spidersql.py
@@ -101,11 +101,8 @@ def main(url,headers):
 '''url和headers可修改'''
 if __name__ == '__main__':
 
-    url='http://10.113.221.244:8003/bas/Account/ashx/SysAshx/DataQuery.ashx?p=GetData&page=1&limit=1000&dbtype=EM_CN&sqlstr=select+*+from+yb_tmp_hwq_01&_=1643014284042'
+    url=''
 
-
-    headers={"Host":"10.113.221.244:8003","Connection":"keep-alive","Accept":"application/json, text/javascript, */*; q=0.01","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36","X-Requested-With":"XMLHttpRequest","Referer":"http://10.113.221.244:8003/bas/Account/SysFile/DataQuery?functionid=0CA6637E6B2CBE8D988863D689162306&r=0.8620768865661128","Accept-Encoding":"gzip, deflate","Accept-Language":"zh-CN,zh;q=0.9,en;q=0.8","Cookie":"ASP.NET_SessionId=ltfchzkglap4lnhcyd53im4c; EM_GX_COOKIE=USER_ID=4Auserid_rad_170171; cityID=12; CITY_COOKIE=cityID=12; userID=4Auserid_rad_170171; userCityID=12; is4AorBass=Bass; DATABASE_COOKIE=DATABASE_ID=9D835CA801CCB07D0890CC0ADED8C9C2; .ASPXAUTH=40A7AAD0A4C934187EE68591090B14C207E10A28591A39ACEDCFFBB89ED256AB93A2EED4372EA33C4C302688982A40EF58A0258C57975C8A2CE651CCF8824D3FAAD56C899E0BEE9511AB00F38ACAD666393F8E06DBE460931728B5559D38D9A67764CC4BAF53C039119B9A451FDB692B280695763E9245051A9887B501F65AF2B11C5AC9C220E4C2FF54B19AE0FFA4175C5B1501BFE95E1DE893E0D2AEFACCE6"}
-
-
+    headers={}
 
     main(url,headers)
