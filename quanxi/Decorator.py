a1e9c6e5a6cbffc714cc972ebfa8eca32df581cf
hwq <a1025747280@qq.com>
2022-08-30 17:51:42 +0800

    format

--- quanxi/Decorator.py
+++ quanxi/Decorator.py
@@ -1,58 +1,62 @@
-#定义一个接口
+# 定义一个接口
 class choose(object):
-    #不实现
+    # 不实现
     def choose_consumer(self):
         pass
 
 
-#定义对象,并且实现接口,原生类，继承接口，同时定义装饰器继承接口，保证原生类不用代码重构
+# 定义对象,并且实现接口,原生类，继承接口，同时定义装饰器继承接口，保证原生类不用代码重构
 
 
-#TODO 假设要增强该类的我号异宽方法，就该用装饰器模式
+# TODO 假设要增强该类的我号异宽方法，就该用装饰器模式
 
 class wkkh_term(choose):
 
-    def __init__(self,res):
+    def __init__(self, res):
 
-        self.res= res
+        self.res = res
+
     def choose_consumer(self):
         print('该对象基本功能')
-        
-    #我号异宽
-    def df_whyk(self,df_tmp):
-        df_tmp_res=df_tmp[(df_tmp['高价值80元以上客户']==self.res[0])&(df_tmp['办理7折提值大于0客户']==self.res[1])&(df_tmp['办理7折提值30-70客户']==self.res[2])&
-        (df_tmp['终端换机']==self.res[3])&(df_tmp['4G资费']==self.res[4])&(df_tmp['预警']==self.res[5])&(df_tmp['不限量']==self.res[6])]
-        return df_tmp_res
-    
-    #我宽异号
-    def df_wkyh(self,df_tmp):
-        df_tmp_res=df_tmp[(df_tmp['我宽异号']==self.res[0])&(df_tmp['是否裸奔高价值']==self.res[1])&(df_tmp['是否老旧设备']==self.res[2])&(df_tmp['免费成员空间']==self.res[3])&(df_tmp['全家享2.0']==self.res[4])]
+
+    # 我号异宽
+    def df_whyk(self, df_tmp):
+        df_tmp_res = df_tmp[(df_tmp['高价值80元以上客户'] == self.res[0]) & (df_tmp['办理7折提值大于0客户'] == self.res[1]) & (df_tmp['办理7折提值30-70客户'] == self.res[2]) &
+                            (df_tmp['终端换机'] == self.res[3]) & (df_tmp['4G资费'] == self.res[4]) & (df_tmp['预警'] == self.res[5]) & (df_tmp['不限量'] == self.res[6])]
         return df_tmp_res
 
-    #全家享
-    def df_qjx(self,df_tmp):
-        df_tmp_res=df_tmp[(df_tmp['我宽异号']==self.res[0])&(df_tmp['是否裸奔高价值']==self.res[1])&(df_tmp['是否老旧设备']==self.res[2])&(df_tmp['免费成员空间']==self.res[3])&(df_tmp['全家享2.0']==self.res[4])]
-        return df_tmp_res
-    
-    #终端购机
-    def df_zdgj(self,df_tmp):
-        df_tmp_res=df_tmp[(df_tmp['是否为宽带客户']==self.res[0])]
+    # 我宽异号
+    def df_wkyh(self, df_tmp):
+        df_tmp_res = df_tmp[(df_tmp['我宽异号'] == self.res[0]) & (df_tmp['是否裸奔高价值'] == self.res[1]) & (
+            df_tmp['是否老旧设备'] == self.res[2]) & (df_tmp['免费成员空间'] == self.res[3]) & (df_tmp['全家享2.0'] == self.res[4])]
         return df_tmp_res
 
-#定义装饰器类，并且实现接口
+    # 全家享
+    def df_qjx(self, df_tmp):
+        df_tmp_res = df_tmp[(df_tmp['我宽异号'] == self.res[0]) & (df_tmp['是否裸奔高价值'] == self.res[1]) & (
+            df_tmp['是否老旧设备'] == self.res[2]) & (df_tmp['免费成员空间'] == self.res[3]) & (df_tmp['全家享2.0'] == self.res[4])]
+        return df_tmp_res
+
+    # 终端购机
+    def df_zdgj(self, df_tmp):
+        df_tmp_res = df_tmp[(df_tmp['是否为宽带客户'] == self.res[0])]
+        return df_tmp_res
+
+# 定义装饰器类，并且实现接口
+
+
 class Decorator(choose):
-    def __init__(self,choose):
-        self.choose=choose
+    def __init__(self, choose):
+        self.choose = choose
+
     def choose_consumer(self):
         pass
 
 
-#实现装饰器，继承装饰类父类，并将choose接口对象注入该类
+# 实现装饰器，继承装饰类父类，并将choose接口对象注入该类
 class extendChoose(Decorator):
-    def __init__(self,choose):
-        self.choose=choose
-    
-
+    def __init__(self, choose):
+        self.choose = choose
 
     def choose_consumer(self):
         print('增强前')
@@ -60,8 +64,8 @@ class extendChoose(Decorator):
         print('增强后')
 
 
-#eg
-res=[1,1,1,1,1,1,1]
-ob=wkkh_term(res)
+# eg
+res = [1, 1, 1, 1, 1, 1, 1]
+ob = wkkh_term(res)
 
 extendChoose(ob).choose_consumer()
--- quanxi/ProxyClass.py
+++ quanxi/ProxyClass.py
@@ -1,4 +1,4 @@
-#定义一个接口
+# 定义一个接口
 from asyncio.windows_events import NULL
 from hashlib import new
 from msilib.schema import SelfReg
@@ -6,54 +6,55 @@ from types import new_class
 
 
 class choose(object):
-    #不实现
+    # 不实现
     def choose_consumer(self):
         pass
 
 
-#real_class
+# real_class
 class wkkh_term(choose):
 
-    def __init__(self,res):
-        self=choose
-        self.res= res
+    def __init__(self, res):
+        self = choose
+        self.res = res
+
     def choose_consumer(self):
         print('该对象基本功能')
-        
-    #我号异宽
-    def df_whyk(self,df_tmp):
-        df_tmp_res=df_tmp[(df_tmp['高价值80元以上客户']==self.res[0])&(df_tmp['办理7折提值大于0客户']==self.res[1])&(df_tmp['办理7折提值30-70客户']==self.res[2])&
-        (df_tmp['终端换机']==self.res[3])&(df_tmp['4G资费']==self.res[4])&(df_tmp['预警']==self.res[5])&(df_tmp['不限量']==self.res[6])]
-        return df_tmp_res
-    
-    #我宽异号
-    def df_wkyh(self,df_tmp):
-        df_tmp_res=df_tmp[(df_tmp['我宽异号']==self.res[0])&(df_tmp['是否裸奔高价值']==self.res[1])&(df_tmp['是否老旧设备']==self.res[2])&(df_tmp['免费成员空间']==self.res[3])&(df_tmp['全家享2.0']==self.res[4])]
+
+    # 我号异宽
+    def df_whyk(self, df_tmp):
+        df_tmp_res = df_tmp[(df_tmp['高价值80元以上客户'] == self.res[0]) & (df_tmp['办理7折提值大于0客户'] == self.res[1]) & (df_tmp['办理7折提值30-70客户'] == self.res[2]) &
+                            (df_tmp['终端换机'] == self.res[3]) & (df_tmp['4G资费'] == self.res[4]) & (df_tmp['预警'] == self.res[5]) & (df_tmp['不限量'] == self.res[6])]
         return df_tmp_res
 
-    #全家享
-    def df_qjx(self,df_tmp):
-        df_tmp_res=df_tmp[(df_tmp['我宽异号']==self.res[0])&(df_tmp['是否裸奔高价值']==self.res[1])&(df_tmp['是否老旧设备']==self.res[2])&(df_tmp['免费成员空间']==self.res[3])&(df_tmp['全家享2.0']==self.res[4])]
+    # 我宽异号
+    def df_wkyh(self, df_tmp):
+        df_tmp_res = df_tmp[(df_tmp['我宽异号'] == self.res[0]) & (df_tmp['是否裸奔高价值'] == self.res[1]) & (
+            df_tmp['是否老旧设备'] == self.res[2]) & (df_tmp['免费成员空间'] == self.res[3]) & (df_tmp['全家享2.0'] == self.res[4])]
         return df_tmp_res
-    
-    #终端购机
-    def df_zdgj(self,df_tmp):
-        df_tmp_res=df_tmp[(df_tmp['是否为宽带客户']==self.res[0])]
+
+    # 全家享
+    def df_qjx(self, df_tmp):
+        df_tmp_res = df_tmp[(df_tmp['我宽异号'] == self.res[0]) & (df_tmp['是否裸奔高价值'] == self.res[1]) & (
+            df_tmp['是否老旧设备'] == self.res[2]) & (df_tmp['免费成员空间'] == self.res[3]) & (df_tmp['全家享2.0'] == self.res[4])]
+        return df_tmp_res
+
+    # 终端购机
+    def df_zdgj(self, df_tmp):
+        df_tmp_res = df_tmp[(df_tmp['是否为宽带客户'] == self.res[0])]
         return df_tmp_res
 
 
 class ProxyClass(choose):
     def __init__(self) -> None:
         super().__init__()
-        self.wkkh_term=choose
+        self.wkkh_term = choose
 
     def choose_consumer(self):
         if self.wkkh_term is not NULL:
             print('代理成功')
-            self.wkkh_term==wkkh_term(choose)
-        self,wkkh_term.choose_consumer(self)
-
-
+            self.wkkh_term == wkkh_term(choose)
+        self, wkkh_term.choose_consumer(self)
 
 
 ProxyClass().choose_consumer()
